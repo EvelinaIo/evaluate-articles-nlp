6b9ad86525a3072e671d5fb8cc75e654bb2965d5
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-02 23:50:07 +0200

    removed unecessary output

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,10 +5,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
     module: {
         rules: [
             {
