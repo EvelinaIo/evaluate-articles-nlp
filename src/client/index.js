4741b9124ec3a774842ded61bc2cce5823541821
Evelina Ioannou <evelinaioannou2@gmail.com>
2020-12-04 11:46:49 +0200

    added styiling

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,10 @@ import { UrlIsValid } from './js/URLChecker'
 
 import './styles/resets.scss'
 import './styles/base.scss'
+import './styles/header.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
-import './styles/header.scss'
+
 
 alert("I EXIST")
 console.log("CHANGE!!");
